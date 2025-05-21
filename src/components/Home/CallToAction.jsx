a6ca9dc9bc2d26f9d291ce8007a412fbad02c509
justinstorm1 <justinstorm1@gmail.com>
2025-05-21 10:52:08 -0400

    Delete src directory

--- src/components/Home/CallToAction.jsx
+++
@@ -1,9 +0,0 @@
-import './CallToAction.css';
-
-export default function CallToAction() {
-    return (
-        <div className="call-to-action">
-            <span>Our Journey Begins Now!</span>
-        </div>
-    )
-}
